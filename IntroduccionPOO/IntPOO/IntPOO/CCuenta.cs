f99d570053067dbad22286b6087bca2d0b450348
David Cano <shiftybody@gmail.com>
2021-10-24 23:54:48 -0500

    firstApproach

---
+++ IntroduccionPOO/IntPOO/IntPOO/CCuenta.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+class CCuenta
+{
+    private string nombre;
+    private string cuenta;
+    private double saldo;
+    private double tipoDeInteres;
+    
+    
+    // en c# utilizamos value como parametro implicito con el mismo tipo que el atributo.
+    // si la propiedad/atributo es de solo lectura o solo escritura se pueden escribir por separado;
+    
+    // una interfaz: son los medios de comunicacion con otro objeto y se refiere al conjunto de metodos
+    // con los cuales se comunica
+    public void asgniarNombre(string nom)
+    {
+        if (nom.Length == 0)
+        {
+            Console.WriteLine("Error: cadena vacia");
+        }
+
+        nombre = nom;
+    }
+
+    public string obtenerNombre()
+    {
+        return nombre;
+    }
+
+    public void asgniarCuenta(string cue)
+    {
+        if (cue.Length == 0)
+        {
+            Console.WriteLine("Error: cuenta no valida");
+            return;
+        }
+
+        cuenta = cue;
+    }
+
+    public string obtenerCuenta()
+    {
+        return cuenta;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+
+    public void ingreso(double cantidad)
+    {
+        if (cantidad < 0)
+        {
+            Console.WriteLine("Error; cantidad negativa");
+            return;
+        }
+
+        saldo += cantidad;
+    }
+    
+    public void reintegro(double cantidad)
+    {
+        if (saldo - cantidad < 0)
+        {
+            Console.WriteLine("Error; no dispone de saldo");
+            return;
+        }
+
+        saldo -= cantidad;
+    }
+    
+    public void asignarTipoDeInteres(double tipo)
+    {
+        if (tipo < 0)
+        {
+            Console.WriteLine("Error; tipo no valido");
+            return;
+        }
+
+        tipoDeInteres = tipo;
+    }
+
+    public double obtenerTipoDeInteres()
+    {
+        return tipoDeInteres;
+    }
+    
+
+
+}
---
+++ IntroduccionPOO/IntPOO/IntPOO/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        // se crea/instancia un objeto de la clase cuenta referenciado en la variable cuenta01
+        CCuenta cuenta01 = new CCuenta();
+        cuenta01.asgniarNombre("Un nombre");
+        cuenta01.asgniarCuenta("Una cuenta");
+        cuenta01.asignarTipoDeInteres(2.5);
+        
+        cuenta01.ingreso(12000);
+        cuenta01.reintegro(3000);
+        
+        // para acceder a un miembro(metodo-atributo) de un objeto se utiliza la sintaxis
+        // nombreObjeto.nombreMetodo
+        Console.WriteLine(cuenta01.obtenerNombre());
+        Console.WriteLine(cuenta01.obtenerCuenta());
+        Console.WriteLine(cuenta01.estado());
+        Console.WriteLine(cuenta01.obtenerTipoDeInteres());
+        
+        // consideraciones: 
+        // cualquier método de una clase tiene acceso es decir, puede incovar,
+        // a todos los miembros de su clase
+        // ---
+        // un objeto de una clase solo puede invocar a metodos de su clase
+        // es decir solo responde a los mensajes para los que ha sido programado
+    }
+}
